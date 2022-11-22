d0733f8f5d3ce1aaa3744b257b2fc621bd8f63ea
Sylvain Luiset <syluiset@z4r8p1.42lyon.fr>
2022-11-22 16:26:37 +0100

    changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,12 @@
 /*   By: syluiset <syluiset@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 14:03:54 by syluiset          #+#    #+#             */
-/*   Updated: 2022/11/22 15:45:30 by syluiset         ###   ########.fr       */
+/*   Updated: 2022/11/22 16:25:23 by syluiset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+#include <stdio.h>
 // int	ft_size_of_lign(int fd)
 // {
 // 	int count;
@@ -26,9 +26,78 @@
 // 	}
 // 	return (0);
 // }
-char	*ft_empty(char *str, char buf)
+int ft_strlen(char const *s)
 {
+	int i;
 
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (0);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = i;
+	i = 0;
+	while (s2[i])
+	{
+		str[j] = s2[i];
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	size_t	len;
+	char	*dup;
+
+	dup = NULL;
+	i = 0;
+	len = ft_strlen(s);
+	dup = malloc(sizeof(char) * len + 1);
+	if (!dup)
+		return (NULL);
+	while (s[i])
+	{
+		dup[i] = (char)s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+void	ft_empty(char *buf, char *str)
+{
+	char *temp;
+
+	printf("%s", ft_strdup(buf));
+	if (str == NULL)
+		str = ft_strdup(buf);
+	else
+	{
+		temp = ft_strdup(buf);
+		str = ft_strjoin(str, temp);
+		free(temp);
+	}
 }
 
 char	*get_next_line(int fd)
@@ -37,6 +106,18 @@ char	*get_next_line(int fd)
 	int		nblu;
 	char	*str_to_return;
 
+	str_to_return = NULL;
 	nblu = read(fd, &buf, BUFFER_SIZE);
-	
+	ft_empty(buf, str_to_return);
+	return (str_to_return);
+}
+
+
+#include <fcntl.h>
+int main()
+{
+	int fd;
+
+	fd = open("bible.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
 }
